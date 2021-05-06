32162873c9a15996b9dc7d128209aad6fb65b268
Rushton Davis-Hall <rdavishall@gmail.com>
2021-05-06 16:46:20 +0000

    Added logical operator examples

--- conditionals.js
+++ conditionals.js
@@ -24,7 +24,7 @@ if (cold) {
 
 //else if statement
 
-const temperature = 12;
+/*const temperature = 12;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -34,4 +34,24 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!");*/
+
+
+//Logical Operators
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+let temperature = -50;
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+let raining = false;
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
