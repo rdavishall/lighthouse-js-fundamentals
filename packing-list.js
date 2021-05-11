7138e3c9fbce7cd5655b312e51f2c589c0f2862a
Rushton Davis-Hall <rdavishall@gmail.com>
2021-05-11 16:12:17 +0000

    while loop

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,17 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+//for loop
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 packingList[3];
+
+
+//while loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
