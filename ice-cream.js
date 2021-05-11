8e659ccbeddf09f6968dbf996ba2ffbf8a9e2e6b
Rushton Davis-Hall <rdavishall@gmail.com>
2021-05-11 16:10:07 +0000

    Array activities

---
+++ ice-cream.js
@@ -0,0 +1,19 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+//add "root beer"
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//get first flavor
+console.log(iceCreamFlavours[0]);
+
+//get the last flavor
+console.log(iceCreamFlavours[5]);
+
+//get total number of flavors
+console.log(iceCreamFlavours.length);
+
